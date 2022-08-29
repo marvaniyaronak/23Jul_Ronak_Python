bd6a59a938795b2cf611398a06160ae336bb75e2
marvaniyaronak <ronakmarvaniya78@gmail.com>
2022-08-29 11:37:50 +0530

    modul2

---
+++ modul-1/syp.py
@@ -0,0 +1,19 @@
+#the syping pogram using thard number 
+'''a=int(input("enter your value :"))
+b=int(input("enter your value :"))
+c=a
+a=b
+b=c
+print("the a value is :",a)
+print("the b value is :",b)'''
+
+
+
+x=int(input("enter your value :"))
+y=int(input("enter your value :"))
+
+x=x+y
+y=x-y
+x=x-y
+print("the x value is :",x)
+print("the y value is :",y)
